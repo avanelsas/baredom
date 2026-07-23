24bbf97603580895ad0214b6391c6f52583ad997
Alexander van Elsas <58037137+avanelsas@users.noreply.github.com>
2026-07-23 11:27:57 +0200

    Add Clojars packaging for BareBuild (#281)
    BareBuild has never been published anywhere. This adds a deps.edn declaring the product sources plus a dependency
    on BareDOM 3.4.0, and a build.clj mirroring the root project's, scoped to publish only
    src/barebuild under com.github.avanelsas/barebuild with an scm tag of barebuild-v0.1.0.

---
+++ barebuild/build.clj
@@ -0,0 +1,45 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]))
+
+(def lib       'com.github.avanelsas/barebuild)
+(def version   "0.1.0")
+(def class-dir "target/classes")
+(def jar-file  (format "target/%s-%s.jar" (name lib) version))
+(def pom-file  (format "target/classes/META-INF/maven/%s/%s/pom.xml"
+                       (namespace lib) (name lib)))
+
+(def basis (delay (b/create-basis {:project "deps.edn"})))
+
+(defn jar [_]
+  (println "Building" jar-file "...")
+  (b/delete {:path class-dir})
+  (b/write-pom {:class-dir class-dir
+                :lib       lib
+                :version   version
+                :basis     @basis
+                :src-dirs  ["src"]
+                :scm       {:url                "https://github.com/avanelsas/baredom"
+                            :connection          "scm:git:git://github.com/avanelsas/baredom.git"
+                            :developerConnection "scm:git:ssh://git@github.com/avanelsas/baredom.git"
+                            :tag                (str "barebuild-v" version)}
+                :pom-data  [[:description "Server-resource runtime for BareDOM — drives presentational web components from server state without client-side business logic. Reads and writes."]
+                            [:url "https://github.com/avanelsas/baredom/tree/main/barebuild"]
+                            [:licenses
+                             [:license
+                              [:name "MIT License"]
+                              [:url "https://opensource.org/licenses/MIT"]]]
+                            [:developers
+                             [:developer
+                              [:name "Alexander van Elsas"]]]]})
+  (b/copy-dir {:src-dirs   ["src"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file  jar-file})
+  (b/copy-file {:src    pom-file
+                :target "pom.xml"})
+  (println "Done:" jar-file))
+
+(defn deploy [_]
+  (jar nil)
+  (println "Deploying to Clojars...")
+  (b/process {:command-args ["clojure" "-X:deploy"]}))
