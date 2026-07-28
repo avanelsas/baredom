720eb948589326cbb523e82cc8892be01d5761ad
Alexander van Elsas <58037137+avanelsas@users.noreply.github.com>
2026-07-28 10:28:31 +0200

    Extend the BareReplay dock with transport controls, an event log, and a data pane (#296)
    * The BareReplay recorder now responds to all live events
    
    * Adds a now button to jump to the now
    
    * Adds a clickable timeline to the replay panel
    
    * Extend the floating replay dock on x-floating-panel with transport controls and a data pane

--- barereplay/test/barereplay/label_test.cljs
+++ barereplay/test/barereplay/label_test.cljs
@@ -11,3 +11,25 @@
     (is (str/starts-with? (label/readout entries 2) "LIVE 2 / 2"))
     (is (str/starts-with? (label/readout entries 1) "REPLAYING 1 / 2"))
     (is (= "REPLAYING 0 / 2" (label/readout entries 0)))))
+
+(deftest detail-at-extracts-request-and-response
+  (let [entries [{:event   [:submit-write {:op :delete :id 7}]
+                  :effects [[:notify-consumers {}] [:write {:method "DELETE" :url "/api/tasks/7"}]]}
+                 {:event   [:response {:outcome :accepted}]
+                  :effects [[:notify-consumers {}]]}
+                 {:event   [:connected {}]
+                  :effects [[:notify-consumers {}]]}]]
+    (is (nil? (label/detail-at entries 0)))
+    (is (= {:request {:method "DELETE" :url "/api/tasks/7"}} (label/detail-at entries 1)))
+    (is (= {:response {:outcome :accepted}} (label/detail-at entries 2)))
+    (is (nil? (label/detail-at entries 3)))))
+
+(deftest item-status-marks-past-present-future
+  (is (= "complete" (label/item-status 1 3)))
+  (is (= "active" (label/item-status 3 3)))
+  (is (= "pending" (label/item-status 4 3))))
+
+(deftest clamp-bounds
+  (is (= 0 (label/clamp -2 0 5)))
+  (is (= 5 (label/clamp 9 0 5)))
+  (is (= 3 (label/clamp 3 0 5))))
